623d98f9473b74291c4c7f558356b657514cf5bd
Javier Vargas <thebruh@Javiers-MacBook-Air.local>
2022-08-02 08:51:51 -0400

    mess with html lava lamp proj

---
+++ README.md
@@ -0,0 +1,9 @@
+# SVG Lava Lamp
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/chrisgannon/pen/QjKXXe](https://codepen.io/chrisgannon/pen/QjKXXe).
+
+Sit back and relax in the glow of one of the better things to come out of the 1960s.
+
+Learn how to build this [here](https://codepen.io/chrisgannon/post/how-to-make-an-svg-lava-lamp). 
+
+Inspired by [this](https://dribbble.com/shots/2058739-it-s-a-lava-lamp)
---
+++ dist/script.js
@@ -0,0 +1,40 @@
+var xmlns = "http://www.w3.org/2000/svg",
+xlinkns = "http://www.w3.org/1999/xlink",   
+  select = function(s) {
+    return document.querySelector(s);
+  },
+  selectAll = function(s) {
+    return document.querySelectorAll(s);
+  },
+  container = select('.container'),
+  mainSVG = select('.mainSVG'),
+  lamp = MorphSVGPlugin.convertToPath('#lampTop')
+
+
+TweenMax.set('svg', {
+  visibility: 'visible'
+})
+
+var tl = new TimelineMax();
+
+
+for(var i = 0; i < 5; i++){
+  
+  var t = TweenMax.to(select('.blob' + i), randomBetween(14, 50), {
+    y:260,
+    repeat:-1,
+    repeatDelay:randomBetween(1, 3),
+    yoyo:true,
+    ease:Linear.easeNone
+  })
+  
+  tl.add(t, (i+1)/0.6)
+}
+
+
+tl.seek(100)
+
+function randomBetween(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
